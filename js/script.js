b95333b5415ba56f9c34cbaac0d3fe1d01156d6a
joshua <javanmega0@gmail.com>
2019-02-22 16:27:16 +0300

    new variables added to the js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-//Business backend logic.
+//Interface logic
+
 $(document).ready(function(){
 $("form#logik").submit(function(event) {
+event.preventDefault();
+ var number1 = parseInt ($("input:radio[name=html]:checked").val());
+ var number2 = parseInt ()
+ var number3 = parseInt ()
+ var number4 = parseInt ()
+ var number5 = parseInt ()
+ var number6 = parseInt ()
+ var number7 = parseInt ()
