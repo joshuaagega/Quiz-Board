833e28989a3aeee6bf9bf6434d62810667299a5a
joshua <javanmega0@gmail.com>
2019-02-22 16:53:20 +0300

    radio inputs have been added

--- js/script.js
+++ js/script.js
@@ -4,9 +4,5 @@ $(document).ready(function(){
 $("form#logik").submit(function(event) {
 event.preventDefault();
  var number1 = parseInt ($("input:radio[name=html]:checked").val());
- var number2 = parseInt ()
- var number3 = parseInt ()
- var number4 = parseInt ()
- var number5 = parseInt ()
- var number6 = parseInt ()
- var number7 = parseInt ()
+ var number2 = parseInt ($("input:radio[name=css]:checked").val());
+ var number3 = parseInt ($("input:radio[name=JavaScript]:").val());
