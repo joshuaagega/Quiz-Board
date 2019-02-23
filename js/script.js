03d306966cad7e966de66c7e8fce9204cd0592e7
joshua <javanmega0@gmail.com>
2019-02-23 19:02:33 +0300

    output value changed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var number1 = parseInt($("input:radio[name=html]:checked").val());
     var number2 = parseInt($("input:radio[name=css]:checked").val());
     var number3 = parseInt($("input:radio[name=javascript]:checked").val());
-    var finalnumber = parseInt((number1 + number2 + number3+ array) / 60 * 100);
-    $("output1").text("display"+"you got;")
+    var finalnumber = parseInt((number1 + number2 + number3) / 60 * 100);
+    $("output1").text("finalnumber"+"you got;")
   });
 });
