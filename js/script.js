ca73b9cdc203fbdb36cf80bd9b397eb6091bd75d
joshua <javanmega0@gmail.com>
2019-02-23 17:34:51 +0300

    double quotation marks have been removed and replaced with single quotation marks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Interface logic
 
-$(document).ready(function() {
-  $("form#logik").submit(function(event) {
+$(document).ready(function () {
+  $('form#logik').submit(function (event) {
     event.preventDefault();
-    var number1 = parseInt($("input:radio[name=html]:checked").val());
-    var number2 = parseInt($("input:radio[name=css]:checked").val());
-    var number3 = parseInt($("input:radio[name=JavaScript]:checked").val());
+    var number1 = parseInt($('input:radio[name=html]:checked').val());
+    var number2 = parseInt($('input:radio[name=css]:checked').val());
+    var number3 = parseInt($('input:radio[name=JavaScript]:checked').val());
     var finalnumber = parseInt((number1 + number2 + number3) / 60 * 100);
-    $("#output1").text(finalnumber);
+    $('#output1').text(finalnumber);
   });
 });
