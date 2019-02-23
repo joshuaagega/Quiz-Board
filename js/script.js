5b75b687adbaa6675f18c414a0717783acd767fa
joshua <javanmega0@gmail.com>
2019-02-23 18:12:30 +0300

    unordered  list replaced with ordered list

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Interface logic
 
 $(document).ready(function () {
-  $('form#logik').submit(function (event) {
+  $("form#logik").submit(function (event) {
     event.preventDefault();
-    var number1 = parseInt($('input:radio[name=html]:checked').val());
-    var number2 = parseInt($('input:radio[name=css]:checked').val());
-    var number3 = parseInt($('input:radio[name=JavaScript]:checked').val());
-    var finalnumber = parseInt((number1 + number2 + number3) / 60 * 100);
-    $('#output1').text(finalnumber);
+    var number1 = parseInt($("input:radio[name=html]:checked").val());
+    var number2 = parseInt($("input:radio[name=css]:checked").val());
+    var number3 = parseInt($("input:radio[name=javascript]:checked").val());
+    var finalnumber = parseInt((number1 + number2 + number3+ array) / 60 * 100);
+    $("output1").text("display"+"you got;")
   });
 });
