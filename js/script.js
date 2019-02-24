fa5858a6fd3eab13131f086adae6bdb9005175c1
joshua <javanmega0@gmail.com>
2019-02-24 11:40:51 +0300

    jquery sign removed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var number1 = parseInt($("input:radio[name=html]:checked").val());
     var number2 = parseInt($("input:radio[name=css]:checked").val());
     var number3 = parseInt($("input:radio[name=javascript]:checked").val());
-    var finalnumber = parseInt($(number1 + number2 + number3));
+    var finalnumber = parseInt((number1 + number2 + number3));
     $("output1").text("finalnumber");
   });
 });
