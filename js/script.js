b42b19b59f3b6b78fac9b9e323631f86e7758f6a
joshua <javanmega0@gmail.com>
2019-02-25 11:48:34 +0300

    comments on percentages have been done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var maxi=40
-var total=100
-var percentage=function(finalnumber,maxi,total){
-return (finalnumber/maxi)*total
+var maxi = 40
+var total = 100
+var percentage = function(finalnumber, maxi, total) {
+  return (finalnumber / maxi) * total
 }
-$(document).ready(function () {
-  $("form#logik").submit(function (event) {
+$(document).ready(function() {
+  $("form#logik").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("input:radio[name=html]:checked").val());
     var number2 = parseInt($("input:radio[name=css]:checked").val());
@@ -12,18 +12,18 @@ $(document).ready(function () {
     var number4 = parseInt($("input:radio[name=html2]:checked").val());
     var finalnumber = parseInt(number1 + number2 + number3 + number4);
     $("#output1").text(finalnumber);
-    var scores=percentage(finalnumber,maxi,total)
+    var scores = percentage(finalnumber, maxi, total)
     $("#output1").text(scores + " %");
 
-    if (percentage=75) {
+    if (scores >= 75) {
       $("#output2").text("EXCELLENT");
-    } else if (percentage >= 65 ) {
+    } else if (scores <= 65) {
       $("#output2").text("AVERAGE");
-    } else if (percentage >= 50 ) {
-      $("#output2").text("FAIL");
+    } else if (scores >= 50) {
+      $("#output2").text("FAIR");
     } else {
       $("output2").text("FAIL");
     }
 
-});
+  });
 });
