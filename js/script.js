86f3276a98dfef790bfd2d8733d8943fe4d2062d
joshua <javanmega0@gmail.com>
2019-02-25 10:31:55 +0300

    now the percentage works

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-//Business Logic.
-var total = function(number1 + number2 + number3 + number4){ return(number1 + number2 +number4)
-};
-var total =40;
-var maximum= 100;
-var percentage=function(finalnumber,total,maximum){
-return(finalnumber/total)*maximum
-};
-
-//Interface logic
-
-$(document).ready(function() {
-  $("form#logik").submit(function(event) {
+var maxi=40
+var total=100
+var percentage=function(finalnumber,maxi,total){
+return (finalnumber/maxi)*total
+}
+$(document).ready(function () {
+  $("form#logik").submit(function (event) {
     event.preventDefault();
     var number1 = parseInt($("input:radio[name=html]:checked").val());
     var number2 = parseInt($("input:radio[name=css]:checked").val());
     var number3 = parseInt($("input:radio[name=javascript]:checked").val());
     var number4 = parseInt($("input:radio[name=html2]:checked").val());
-    var finalnumber = parseInt((number1 + number2 + number3 + number4));
+    var finalnumber = parseInt(number1 + number2 + number3 + number4);
     $("#output1").text(finalnumber);
-
-  });
+    var scores=percentage(finalnumber,maxi,total)
+    $("#output1").text(scores + " %");
+});
 });
