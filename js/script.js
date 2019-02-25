b976429fe8985704a6479dcd67407977fa12890c
joshua <javanmega0@gmail.com>
2019-02-25 08:00:33 +0300

    new question was added on html , a variable named number4 was set for it at js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ $(document).ready(function () {
     var number2 = parseInt($("input:radio[name=css]:checked").val());
     var number3 = parseInt($("input:radio[name=javascript]:checked").val());
     var finalnumber = parseInt((number1 + number2 + number3));
-    $("#output1").text("finalnumber");
+    $("#output1").text(finalnumber);
+
   });
 });
+// function calcScore() {
+//   var score = 0;
+//   $(".quiz:checked").each(fuction() {
+//     score += parseInt($(this).val(), 10);
+//   });
+//   $("div[id=output1]").val(score)
+// }
+// $().ready(function() {
+//   $(".quiz").change(function() {
+//     calcScore()
+//   });
+// });
