7edd1802d5bb6c0384e51e89f8faad9e8b3a6fd3
joshua <javanmega0@gmail.com>
2019-02-25 08:04:58 +0300

    one closing bracket has been removed at the var finalnumber now results are in total

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
     var number1 = parseInt($("input:radio[name=html]:checked").val());
     var number2 = parseInt($("input:radio[name=css]:checked").val());
     var number3 = parseInt($("input:radio[name=javascript]:checked").val());
-    var finalnumber = parseInt((number1 + number2 + number3));
+    var number4 = parseInt($("input:radio[name=html2]:checked").val());
+    var finalnumber = parseInt((number1 + number2 + number3 + number4);
     $("#output1").text(finalnumber);
 
   });
